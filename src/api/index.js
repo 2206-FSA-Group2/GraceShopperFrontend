ac4c5c04e5060931e8211f0cfffbf4af2e49e5b0
Christian Factoriza <cfactoriza@gmail.com>
2022-08-12 13:03:25 -0500

    First pass at Register.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,52 @@
 import axios from "axios";
-
 const BASE = "https://whispering-peak-79661.herokuapp.com/api/";
+
+export async function registerUser(email, password, firstName, lastName, isActive, isAdmin) {
+    try {
+      const response = await fetch(`${BASE}users/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          firstName: firstName,
+          lastName: lastName,
+          isActive: isActive,
+          isAdmin: isAdmin,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error('Something went Wrong');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error.messsage);
+    }
+  }
+  
+  export async function loginUser(email, password) {
+    try {
+      const response = await fetch(`${BASE}api/users/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error('Something went Wrong');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error.messsage);
+    }
+  }
+
+  
---
+++ src/components/Register.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState ('');
+    const [firstName, setFirstName] = useState ('');
+    const [lastName, setLastName] = useState ('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [error, setError] = useState(false);
+
+    async function registerUserSubmit(event){
+        event.preventDefault();
+        if (password.length < 8) {
+          setErrorMessage('');
+          setPasswordError('Password must be 8 characters or longer');
+          setError(true);
+          return;
+        }
+        let isActive = true;
+        let isAdmin = false;
+        const registrationInfo = await registerUser(email, password, firstName, lastName, isActive, isAdmin);
+        if (!registrationInfo) {
+          setPasswordError('');
+          setErrorMessage('That e-mail has been taken');
+          setError(true);
+          return;
+        }
+        setEmail('');
+        setPassword('');
+        setFirstName('');
+        setLastName('');
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={registerUserSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setError(false);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setError(false);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+            setError(false);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+            setError(false);
+          }}
+        />
+        <button type = "submit">Register</button>
+        {errorMessage && error ? <h1>{errorMessage}</h1> : null}
+      </form>
+        </div>
+    )
+}
+
+export default Register
