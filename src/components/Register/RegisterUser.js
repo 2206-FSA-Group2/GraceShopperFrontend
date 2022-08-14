4c70e9f31c8cdcbc5f9264a4be6ba55a5eecb534
Christian Factoriza <cfactoriza@gmail.com>
2022-08-14 16:39:31 -0500

    Bootstrapped Register

--- src/components/Register/RegisterUser.js
+++ src/components/Register/RegisterUser.js
@@ -53,49 +53,79 @@ const RegisterUser = () => {
 
 
     return (
-        <div>
-            <form onSubmit={registerUserSubmit}>
-        <input
-          type="text"
-          placeholder="E-mail"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-            setError(false);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-            setError(false);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="First Name"
-          value={firstName}
-          onChange={(e) => {
-            setFirstName(e.target.value);
-            setError(false);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={lastName}
-          onChange={(e) => {
-            setLastName(e.target.value);
-            setError(false);
-          }}
-        />
-        <button type = "submit">Register</button>
-        {errorMessage && error ? <h1>{errorMessage}</h1> : null}
-      </form>
-        </div>
-    )
-}
+      <section
+      style = {{
+    backgroundImage:"url('https://wallpaperaccess.com/full/3951115.jpg')",
+    backgroundSize: "cover",
+    backgroundRepeat: 'no-repeat'
+  }}>
 
+  <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5" >
+    <div className="row gx-lg-5 align-items-center mb-5">
+      <div className="col-lg-6 mb-5 mb-lg-0">
+        <h1 className="my-5 display-5 fw-bold ls-tight" >
+          The best vintages <br />
+          <span>for your experience</span>
+        </h1>
+        <h3 className="mb-4 opacity-70">
+          Bringing the technology of Yesterday to you Tomorrow!
+        </h3>
+      </div>
+
+      <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
+        <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+        <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
+
+        <div className="card bg-glass">
+          <div className="card-body px-4 py-5 px-md-5">
+            <form onSubmit={registerUserSubmit}>
+              <div className="row">
+                <div className="col-md-6 mb-4">
+                  <div className="form-outline">
+                    <input type="text" id="form3Example1" value={firstName} onChange ={(e)=>{
+                      setFirstName(e.target.value);
+                      setError(false);
+                    }} className="form-control" />
+                    <label className="form-label" htmlFor="form3Example1">First name</label>
+                  </div>
+                </div>
+                <div className="col-md-6 mb-4">
+                  <div className="form-outline">
+                    <input type="text" id="form3Example2" value={lastName} onChange ={(e)=>{
+                      setLastName(e.target.value);
+                      setError(false);
+                    }} className="form-control" />
+                    <label className="form-label" htmlFor="form3Example2">Last name</label>
+                  </div>
+                </div>
+              </div>
+
+              <div className="form-outline mb-4">
+                <input type="email" value={email} onChange={(e) => {
+                setEmail(e.target.value);
+                setError(false);
+           }} id="form3Example3" className="form-control" />
+                <label className="form-label" htmlFor="form3Example3">Email address</label>
+              </div>
+
+              <div className="form-outline mb-4">
+                <input type="password" value = {password} onChange={(e)=>{
+                  setPassword(e.target.value);
+                  setError(false);
+                }} id="form3Example4" className="form-control" />
+                <label className="form-label" htmlFor="form3Example4">Password</label>
+              </div>
+
+              <button type="submit" className="btn btn-primary btn-block mb-4" >
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    )
+              }
 export default RegisterUser
