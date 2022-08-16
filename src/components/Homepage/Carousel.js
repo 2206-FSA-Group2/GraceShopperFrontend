de3a246665ea331590291028818fa59d7e9a034a
Christian Factoriza <cfactoriza@gmail.com>
2022-08-16 13:57:43 -0500

    Messing with Carousel

--- src/components/Homepage/Carousel.js
+++ src/components/Homepage/Carousel.js
@@ -1,38 +1,33 @@
 import  React  from "react";
 
 const Carousel = () => {
-
+let myCarousel = document.querySelector('MyCarousel')
 
     return(
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block w-100" src="https://placeimg.com/1080/500/animals" alt="First slide"></img>
-            <div class="carousel-caption d-none d-md-block">
-                <h5>My Caption Title (1st Image)</h5>
-                <p>The whole caption will only show up if the screen is at least medium size.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="https://placeimg.com/1080/500/arch" alt="Second slide"></img>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="https://placeimg.com/1080/500/nature" alt="Third slide"></img>
-        </div>
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel" data-bs-interval='5000'>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://www.wallpaperup.com/uploads/wallpapers/2014/03/30/315240/f6a86d5cfe3709663042b5482ec07336-700.jpg" className="d-block w-100" alt="First image"></img>
+      {/* <div className="carousel-caption ">
+        <h5>First slide label</h5>
+        <p>Lorem ipsum this price</p>
+      </div> */}
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+    <div className="carousel-item">
+      <img src="https://i.pinimg.com/736x/b3/74/db/b374db6c8c02441212f820bf36f51c42--wallpaper-backgrounds-vintage-retro.jpg" className="d-block w-100" alt="Second image"></img>
+      {/* <div className="carousel-caption ">
+        <h5>Second slide label</h5>
+        <p>Last one in stock</p>
+      </div> */}
+    </div>
+    <div className="carousel-item">
+      <img src="https://c0.wallpaperflare.com/preview/776/812/924/blurred-background-close-up-electronics-equipment-thumbnail.jpg" className="d-block w-100" alt="Third image"></img>
+      {/* <div className="carousel-caption ">
+        <h5>Third slide label</h5>
+        <p>This is a steal!</p>
+      </div> */}
+    </div>
+  </div>
 </div>
     )
 
