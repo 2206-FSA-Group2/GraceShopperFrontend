04a912605c0aca5fa97a256f83b7afbfa94ae993
Christian Factoriza <cfactoriza@gmail.com>
2022-08-17 10:36:24 -0500

    Stylized and finished CarouselCards

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,13 +2,14 @@ import  React  from "react";
 import { Link, useNavigate } from 'react-router-dom'
 import  Navbar  from '../Navbar/Navbar'
 import Carousel from "./Carousel";
+import CarouselCards from "./CarouselCards";
 import SalesCarousel from "./SalesCarousel";
 
 
 const Homepage = () => {
 
     return (
-        <div>
+        <div >
         <header>
             <label>Search</label>
             <input></input>
@@ -18,20 +19,11 @@ const Homepage = () => {
         </header>
         <div>
             <Navbar/>
-        <div>
+        </div>
             <h1>Welcome to NAME TO BE DEFINED</h1>
-        </div>
-<section>
-        <div>
-            <Carousel/>
-        </div>
-</section>
-<section>
-        <div>
-            <SalesCarousel/>
-        </div>
-</section>
-        </div>
+            <div style={{backgroundColor: '#3B6978'}}>
+              <CarouselCards/>
+            </div>
         </div>
         )
 }
