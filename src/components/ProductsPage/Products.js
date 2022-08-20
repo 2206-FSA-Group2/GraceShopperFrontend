9a42503a4728fb1f9436a234dec937334260807b
rafaelbchs <rafaelbchacins@gmail.com>
2022-08-20 15:34:15 -0500

    added wishlist page

--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
-import { getAllCategories, getAllProducts, addProductToCart } from "../../api";
+import { getAllCategories, getAllProducts, addProductToCart, addItemToWishlist } from "../../api";
 
 import FilterBox from "./FilterBox";
 
@@ -13,6 +13,7 @@ import LoadingScreen from "../LoadingPage/LoadingScreen";
 const Products = (props) => {
   const {categoriesData, setCategoriesData, productsData, setProductsData, searchProduct, setSearchProduct, stateRefresh, setStateRefresh} = props
   const [loading, setLoading] = useState(true)
+  const token = localStorage.getItem('token')
 
   const [cartProduct, setCartProduct] = useState(null)
   let selectedProduct={};
@@ -25,6 +26,11 @@ const Products = (props) => {
     console.log("cart items after add", localStorage.getItem("cartItems"))
     console.log(selectedProduct.id);
     } catch(error) {throw error}
+  }
+
+  async function handleAddItemToWishlist(event){
+    const newWishedItem = await addItemToWishlist(token, selectedProduct.id)
+    console.log(newWishedItem)
   }
 
   useEffect(() => {
@@ -88,6 +94,10 @@ const Products = (props) => {
               <small className="text-muted">Items on stock: {product.quantity_on_hand}</small>
               <div className="fact-line">
                 <span className="fact-name">${product.price}</span>
+                {
+                  token && <button type="button" className="btn bi bi-bookmark-plus btn-secondary btn-sm" onClick={handleAddItemToWishlist} onMouseDown={(e)=>{e.preventDefault();selectedProduct=product}}></button>
+                }
+                
               </div>
             </div>
           );
---
+++ src/components/Wishlist/Wishlist.js
@@ -0,0 +1,98 @@
+import React, {useState, useEffect} from "react";
+import { getProductById, getWishlistItems, removeItemFromWishlist } from "../../api";
+import { Link } from "react-router-dom";
+import { addProductToCart } from "../../api";
+import LoadingScreen from "../LoadingPage/LoadingScreen";
+
+const Wishlist = () => {
+    const [wishlistData, setWishlistData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const token = localStorage.getItem('token')
+    if (!token) return <UnauthorizedRoute />
+    const [counter, setCounter] = useState(0)
+    console.log(wishlistData)
+
+    let selectedProduct={};
+
+    async function addItemToCart(event) {
+        try {
+        event.preventDefault();
+        if(await addProductToCart(selectedProduct)) alert("Added product to cart")
+        } catch(error) {throw error}
+      }
+
+    async function deleteItemFromWishlist(event){
+        try {
+            event.preventDefault();
+            await removeItemFromWishlist(selectedProduct.id, token)
+            setCounter(counter + 1)             
+            } catch(error) {throw error}
+    }
+
+    useEffect(()=>{
+        async function getData() {
+            const data = await getWishlistItems(token)
+            let arrItems = []
+            data.map((product)=>{arrItems.push(product.product_id)})
+            const items = await Promise.all(arrItems.map(getProductById))            
+            setWishlistData(items);
+          }
+          getData();
+          setTimeout(() => setLoading(false), 2000)
+    }, [counter])
+
+    return (<>
+    {
+        loading === false ? <>
+   
+        <h1 style={{textAlign: "center", paddingTop : ".8rem"}}>Your wishlist</h1>
+        <div style={{width: "50%", margin: "0 auto"}}>
+        <section style={{margin: "0 auto"}}>
+        {
+            wishlistData.length > 0 ?
+            wishlistData.map((product, idx)=>{return(
+                
+                <div className="item rounded border" style={{marginTop: "0.4rem"}} key={idx}>
+                <div style= {{display: "inline-flex", justifyContent:"flex-end"}}>
+                <span style={{marginRight: "auto"}}><b>{product.name}</b></span>
+                <button
+                  className="btn bi bi-cart-plus btn-primary btn-sm"
+                  onMouseDown={(e)=>{e.preventDefault();selectedProduct=product}}
+                  onClick={addItemToCart}
+                  style={{width:"5%", margin: ".5rem"}}
+                ></button>
+                <button
+                  className="btn bi bi-bookmark-dash btn-danger btn-sm"
+                  onMouseDown={(e)=>{e.preventDefault();selectedProduct=product}}
+                  onClick={deleteItemFromWishlist}
+                  style={{width:"5%", margin: ".5rem"}}
+                ></button>
+                </div>
+                <img
+                  className="item-image"
+                  src={product.photos[0].url}
+                  alt="product-name"
+                  style={{ padding: '.2rem'}}
+                  width="250"
+                  height="200"
+                />
+              <hr></hr>
+              <p className="item-description">{product.description}</p>
+              <small className="text-muted">Items on stock: {product.quantity_on_hand}</small>
+              <div className="fact-line">
+                <span className="fact-name">${product.price}</span>
+
+              </div>
+            </div>
+            
+            )}) : <h5 style={{margin: "0 auto", paddingTop: ".8rem", textAlign:"center"}}>You've got no items in your wishlist! </h5>
+        }
+        </section>
+        </div>
+        </>
+        : <LoadingScreen />
+    }
+    </>)
+}
+
+export default Wishlist;
