763a92cb89cd3b15da9497fd162ed7908ade2e42
Christian Factoriza <cfactoriza@gmail.com>
2022-08-20 14:09:26 -0500

    Re-organized user Card and set states and variables. Functions pending

---
+++ src/components/Profile/UpdateUser.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { updateUserInfo } from '../../api';
+
+const UpdateUser = (props)=> {
+    const {token, userId, setShowEdit} = props
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const navigate = useNavigate;
+
+    async function handleUpdateAddress(event){
+        event.preventDefault();
+        const updatedUser = await updateUserInfo(token, userId, email, firstName, lastName);
+        setShowEdit(false);
+        console.log(updatedUser)
+        navigate("/profile")
+    }
+
+    return (
+        <form onSubmit = {handleUpdateAddress} >
+            <div className="form-group">
+            <label  className="col-lg-3 control-label">First name</label>
+            <div className="col-lg-8">
+              <input className="form-control" type="text" onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-lg-3 control-label">Last name</label>
+            <div className="col-lg-8">
+              <input className="form-control" type="text" onChange={(e) => setLastName(e.target.value)} />
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-lg-3 control-label">Email</label>
+            <div className="col-lg-8">
+              <input
+                className="form-control"
+                type="text"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+            <button type= "submit">
+            Update User
+            </button>
+        </form>
+        
+    )
+}
+
+export default UpdateUser
---
+++ src/components/Profile/UserCard.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import UpdateUser from './UpdateUser';
+
+const UserCard = (props) => {
+    const {userInformation, user, token} = props
+    const [showEdit, setShowEdit] = useState(false)
+    const userId = user.id
+    async function handleClick(event){
+        event.preventDefault();
+        setShowEdit(true);
+        console.log(showEdit)
+    }
+    return (
+        <div>
+        <div>
+            {!showEdit ? <div>
+        <div className="form-group">
+            <h5  className="col-lg-3 control-label">First name: {userInformation.firstName}</h5>
+          </div>
+          <div className="form-group">
+            <h5 className="col-lg-3 control-label">Last name: {userInformation.lastName}</h5>
+          </div>
+          <div className="form-group">
+            <h5 className="col-lg-3 control-label">Email: {user.email}</h5>
+          </div>
+          <button onClick={handleClick}> Update </button>
+        </div> : null }
+            {showEdit ?<UpdateUser token = {token} userId = {userId} setShowEdit = {setShowEdit}/> : null }
+        </div>
+        </div>
+    )
+}
+
+export default UserCard
