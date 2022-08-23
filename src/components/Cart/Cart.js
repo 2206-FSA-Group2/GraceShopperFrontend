8a609a6aa2e8efacee35c9db006586744dd22683
Eric Loving <ericdloving@gmail.com>
2022-08-22 21:46:16 -0500

    The most important change here was the re-calculation of itemsInCart during the deletion process--Before the array was largely staying in place, just filtering out the dead element; Now, it gets totally rebuilt from two slices concatenated in a new variable, then gets assigned back to itemsInCart, so React can't pretend it's the same array.  Also, the key on the .map children is refined so that there won't be ghost children hanging out in place because an array index still exists.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,12 +8,13 @@ const Cart = ({ productsData }) => {
   const [cartSubtotal, setCartSubtotal] = useState(0);
   const [cartHasBeenUpdated, setCartHasBeenUpdated] = useState(false);
   const [selectedCartItemId, setSelectedCartItemId] = useState(0);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false);
+  const [refresh, setRefresh] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     let subtotal = 0;
-    console.log("Reticulating splines", itemsInCart);
+
     for (let item of itemsInCart) {
       subtotal += item.quantity * item.price;
     }
@@ -30,7 +31,7 @@ const Cart = ({ productsData }) => {
       setCartHasBeenUpdated(true); //to trigger re-renders when necessary
     }
     fetchCart();
-    setTimeout(() => setLoading(false), 2000)
+    // setTimeout(() => setLoading(false), 2000);
   }, []);
 
   function handleCheckoutButton(event) {
@@ -69,10 +70,10 @@ const Cart = ({ productsData }) => {
     setItemsInCart(newItems);
 
     //if the cart is in local storage, update it there too
-    if (localStorage.getItem("cartItems"))
-      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    if (localStorage.getItem("cartItems")) {
+      localStorage.setItem("cartItems", JSON.stringify(newItems));
+    }
     //otherwise, it's a remote cart -- update the quantity in the db
-    updateCartItemQty(selectedCartItemId, newItems[selectedItemIndex].quantity);
   }
 
   function handleDecrementQuantity() {
@@ -89,148 +90,189 @@ const Cart = ({ productsData }) => {
 
     //if the cart is in local storage, update it there too
     if (localStorage.getItem("cartItems"))
-      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      localStorage.setItem("cartItems", JSON.stringify(newItems));
+    updateCartItemQty(selectedCartItemId, newItems[selectedItemIndex].quantity);
   }
 
   async function deleteSelectedItem() {
     try {
+      if (localStorage.getItem("token")) deleteCartItem(selectedCartItemId);
+      
+    } catch (error) {
+      throw error;
+    } finally {
       if (!localStorage.getItem("cartItems")) {
         setItemsInCart(
           itemsInCart.filter((item) => item.cartItemId !== selectedCartItemId)
         );
       } else
-        setItemsInCart(
-          itemsInCart.filter((item) => item.id !== selectedCartItemId)
-        );
-    } catch (error) {
-      throw error;
-    }
-    //if user is logged in, call the api to delete the cartItem from the db
-    if (localStorage.getItem("token")) deleteCartItem(selectedCartItemId);
+        {
+          let index = itemsInCart.findIndex(item=>item.cartItemId === selectedCartItemId);
+          const newItems = itemsInCart.slice(0,index).concat(itemsInCart.slice(index+1,itemsInCart.length))
+          console.log(newItems)
+        
+          setItemsInCart(newItems)
+        }
+        
+    }setCartHasBeenUpdated(!cartHasBeenUpdated);
   }
 
   function handleDeleteItem(event) {
     deleteSelectedItem();
-    setCartHasBeenUpdated(!cartHasBeenUpdated);
     setCartSubtotal(0);
   }
 
   return (
-  //TODO: Add in an editable quantity field in the output
-  <>  
-    {
-      loading === false ? 
-    <div className="bg-image" id="cartPage">
-      <div id="cartInternalHeader">
-        <div id="cartTitle">
-          <h4 style={{ textAlign: "center", paddingTop: ".8rem" }}>
-            Your Cart
-          </h4>
-        </div>
-      </div>
-     
-      {!itemsInCart?.length ? (
-        <h5 style={{margin: "0 auto", paddingTop: ".8rem", textAlign:"center"}}>You've got no items in your cart! </h5>
-      ) : ( <>
-        
-        <div style={{display: "flex", justifyContent: "flex-end", width:"50%", margin: "0 auto"}}>
-      <p style={{marginRight: "2rem", marginBottom: "0", paddingBottom: "0"}} id="cartTotal">Total: <b>${cartSubtotal.toFixed(2)}</b></p>
-            <button
-              type="button"
-              className="btn btn-success"
-              onClick={handleCheckoutButton}
-            >
-              Proceed to Checkout
-            </button>
+    //TODO: Add in an editable quantity field in the output
+    <>
+      {loading === false ? (
+        <div className="bg-image" id="cartPage">
+          <div id="cartInternalHeader">
+            <div id="cartTitle">
+              <h4 style={{ textAlign: "center", paddingTop: ".8rem" }}>
+                Your Cart
+              </h4>
             </div>
-            <div id="cartItems" style={{ width: "50%", margin: "0 auto" }}>
-          {itemsInCart.map((item, idx) => {
-            return (
-              <>
+          </div>
+
+          {!itemsInCart?.length ? (
+            <h5
+              style={{
+                margin: "0 auto",
+                paddingTop: ".8rem",
+                textAlign: "center",
+              }}
+            >
+              You've got no items in your cart!{" "}
+            </h5>
+          ) : (
+            <>
               <div
-                className="item rounded border"
-                style={{ marginTop: "0.4rem", backgroundColor: "#e4eaeb" }}
-                key={idx}
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  width: "50%",
+                  margin: "0 auto",
+                }}
               >
-                <div className="card-body">
-                  <p>
-                    <b>{item.name}</b>
-                  </p>
-                </div>
-
-                <img
-                  className="item-image"
-                  src={
-                    item.photos?.length
-                      ? item.photos[0].url
-                      : item.images[0].url
-                  }
-                  alt={`product ${item.product_id}`}
-                  width="250"
-                  height="200"
-                  style={{ margin: "0 auto" }}
-                />
-
-                <div style={{ display: "flex" }}>
-                  <p style={{ marginRight: "auto" }}>
-                    Quantity: <b>{item.quantity}</b>
-                  </p>
-                  <a
-                    onMouseDown={() =>
-                      setSelectedCartItemId(item.cartItemId || item.id)
-                    }
-                    onClick={handleIncrementQuantity}
-                    style={{
-                      color: "green",
-                      cursor: "pointer",
-                      fontSize: "26px",
-                      padding: "0 0.5rem"
-                    }}
-                  >
-                    <i className="fas fa-solid fa-plus"></i>
-                  </a>
-                  <a
-                    onMouseDown={() =>
-                      setSelectedCartItemId(item.cartItemId || item.id)
-                    }
-                    onClick={handleDecrementQuantity}
-                    style={
-                      item.quantity > 1
-                        ? { color: "red", cursor: "pointer", fontSize: "26px", padding: "0 0.5rem" }
-                        : { color: "cecece", fontSize: "26px", padding: "0 0.5rem" }
-                    }
-                  >
-                    <i className="fas fa-solid fa-minus"></i>
-                  </a>
-
-                  <a
-                    onMouseDown={() =>
-                      setSelectedCartItemId(item.cartItemId || item.id)
-                    }
-                    onClick={handleDeleteItem}
-                    style={{
-                      color: "#cecece",
-                      cursor: "pointer",
-                      fontSize: "26px",
-                      color: "black",
-                      padding: "0 0.5rem"
-                    }}
-                  >
-                    <i className="fas fa-trash-alt"></i>
-                  </a>
-                </div>
+                <p
+                  style={{
+                    marginRight: "2rem",
+                    marginBottom: "0",
+                    paddingBottom: "0",
+                  }}
+                  id="cartTotal"
+                >
+                  Total: <b>${cartSubtotal.toFixed(2)}</b>
+                </p>
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  onClick={handleCheckoutButton}
+                >
+                  Proceed to Checkout
+                </button>
               </div>
-              </>
-            );
-          })}
+              <div id="cartItems" style={{ width: "50%", margin: "0 auto" }}>
+                {itemsInCart.map((item, index) => {
+                  return (
+                    <div key={`cartItem${item.id}.${index}`}>
+                      <div
+                        className="item rounded border"
+                        style={{
+                          marginTop: "0.4rem",
+                          backgroundColor: "#e4eaeb",
+                        }}
+                      >
+                        <div className="card-body">
+                          <p>
+                            <b>{item.name}</b>
+                          </p>
+                        </div>
 
+                        <img
+                          className="item-image"
+                          src={
+                            item.photos?.length
+                              ? item.photos[0].url
+                              : item.images[0].url
+                          }
+                          alt={`product ${item.product_id}`}
+                          width="250"
+                          height="200"
+                          style={{ margin: "0 auto" }}
+                        />
+
+                        <div style={{ display: "flex" }}>
+                          <p style={{ marginRight: "auto" }}>
+                            Quantity: <b>{item.quantity}</b>
+                          </p>
+                          <a
+                            onMouseDown={() =>
+                              setSelectedCartItemId(item.cartItemId || item.id)
+                            }
+                            onClick={handleIncrementQuantity}
+                            style={{
+                              color: "green",
+                              cursor: "pointer",
+                              fontSize: "26px",
+                              padding: "0 0.5rem",
+                            }}
+                          >
+                            <i className="fas fa-solid fa-plus"></i>
+                          </a>
+                          <a
+                            onMouseDown={() =>
+                              setSelectedCartItemId(item.cartItemId || item.id)
+                            }
+                            onClick={handleDecrementQuantity}
+                            style={
+                              item.quantity > 1
+                                ? {
+                                    color: "red",
+                                    cursor: "pointer",
+                                    fontSize: "26px",
+                                    padding: "0 0.5rem",
+                                  }
+                                : {
+                                    color: "cecece",
+                                    fontSize: "26px",
+                                    padding: "0 0.5rem",
+                                  }
+                            }
+                          >
+                            <i className="fas fa-solid fa-minus"></i>
+                          </a>
+
+                          <a
+                            onMouseDown={() =>
+                              setSelectedCartItemId(item.cartItemId || item.id)
+                            }
+                            onClick={handleDeleteItem}
+                            style={{
+                              color: "#cecece",
+                              cursor: "pointer",
+                              fontSize: "26px",
+                              color: "black",
+                              padding: "0 0.5rem",
+                            }}
+                          >
+                            <i className="fas fa-trash-alt"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          )}
         </div>
-        </>
+      ) : (
+        <LoadingScreen />
       )}
-    </div>
-    : <LoadingScreen/>
-    }
-    </> );
+    </>
+  );
 };
 
 export default Cart;
